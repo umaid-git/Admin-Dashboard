20ad38d4f2f4dc01478031a4658fca3ca8ff6457
umaid-git <uxa180002@utdallas.edu>
2023-05-14 23:27:58 -0500

    added line, pie, bar charts

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
 import Form from "./scenes/form";
 import Calendar from "./scenes/calendar";
-// import Bar  from "./scenes/bar"
-// import Line  from "./scenes/line"
-// import Pie  from "./scenes/pie"
+import Bar from "./scenes/bar";
+import Line from "./scenes/line";
+import Pie from "./scenes/pie";
 import FAQ from "./scenes/faq";
 // import Geography  from "./scenes/geography"
 import { Routes, Route } from "react-router-dom";
@@ -34,9 +34,9 @@ function App() {
               <Route path="/form" element={<Form />} />
               <Route path="/calendar" element={<Calendar />} />
               <Route path="/faq" element={<FAQ />} />
-              {/* <Route path="/bar" element={<Bar />} /> */}
-              {/* <Route path="/pie" element={<Pie />} /> */}
-              {/* <Route path="/line" element={<Line />} /> */}
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
               {/* <Route path="/geography" element={<Geography />} /> */}
             </Routes>
           </main>
